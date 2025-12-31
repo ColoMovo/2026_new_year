0b745174ac67ab0c5bb60e8ce97e38da1c0eb9fe
ColoMovo <156437684+ColoMovo@users.noreply.github.com>
2025-12-31 23:24:03 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2026_new_year', // 确保在 GitHub Pages 子目录下资源路径正确
+  base: '/2026_new_year/', // 确保在 GitHub Pages 子目录下资源路径正确
   build: {
     outDir: 'dist',
   }
